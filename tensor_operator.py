52591fe8e54d49bdc2e7bfc809c34ab66f132f11
Lukas <Lukas.Huth@gmx.net>
2022-08-09 13:27:42 +0200

    Further unit test and functionality

--- tensor_operator.py
+++ tensor_operator.py
@@ -1,7 +1,4 @@
-from sympy import Symbol
-from sympy.physics.wigner import clebsch_gordan, wigner_6j, wigner_9j
 from abc import ABC, abstractmethod
-import numpy as np
 
 
 class TensorOperatorInterface(ABC):
@@ -10,14 +7,23 @@ class TensorOperatorInterface(ABC):
     """
 
     def __add__(self, other):
-        self.add(other)
+        return self.add(other)
+
+    def __radd__(self, other):
+        return self.__add__(other)
 
     def __str__(self):
         return self.to_expression()
 
+    def __repr__(self):
+        return self.__str__()
+
     @abstractmethod
-    def __mul__(self, other):
+    def __mul__(self, factor):
         pass
+
+    def __rmul__(self, factor):
+        return self.__mul__(factor)
 
     @abstractmethod
     def to_latex(self):
@@ -37,13 +43,16 @@ class TensorOperatorInterface(ABC):
 
 
 class TensorOperatorComposite(TensorOperatorInterface):
+    """
+
+    """
 
     def __init__(self, *args):
         self.children = [arg for arg in args]
 
-    def __mul__(self, other):
+    def __mul__(self, factor):
         for child in self.children:
-            child *= other
+            child *= factor
 
     def to_latex(self):
         return " + ".join([child.to_latex() for child in self.children])
@@ -52,7 +61,13 @@ class TensorOperatorComposite(TensorOperatorInterface):
         return " + ".join([child.to_expression() for child in self.children])
 
     def add(self, other):
-        self.children.append(other)
+        if isinstance(other, self.__class__):
+            args = self.children + other.children  # This keeps a flat hierarchy
+        else:
+            args = self.children + [other]
+        new_object = self.__class__(*args)
+        new_object._simplify()
+        return new_object
 
     def couple(self, other, rank, factor):
         if isinstance(other, TensorOperatorComposite):
@@ -62,21 +77,44 @@ class TensorOperatorComposite(TensorOperatorInterface):
             new_children = [child.couple(other, rank, factor) for child in self.children]
         else:
             raise (TypeError(f"Cannot couple type {type(other)} to an object of {type(self)}."))
-        return TensorOperatorComposite(new_children)
+        new_object = TensorOperatorComposite(new_children)
+        new_object._simplify()
+        return new_object
+
+    def _simplify(self):
+        children = self.children[::-1]
+        new_children = []
+        while len(children) > 0:
+            child = children.pop(0)
+            for i, other_child in enumerate(children):
+                if child == other_child:
+                    children.pop(i)
+                    child += other_child
+            new_children.append(child)
+        self.children = new_children
 
 
 class TensorOperator(TensorOperatorInterface):
+    """
+
+    """
     CompositeClass = TensorOperatorComposite
 
-    def __init__(self, rank=0, factor=1, space='1', representation='X', tex_representation='X'):
+    def __init__(self, rank=0, factor=1, space='1', representation=None):
         self._rank = rank
         self._factor = factor
         self._space = space
         self._representation = representation
-        self._tex_representation = tex_representation
 
-    def __mul__(self, other):
-        self._factor *= other.factor
+    def __mul__(self, factor):
+        new_factor = self.factor * factor
+        return self.__class__(self.rank, new_factor, self.space, self.representation)
+
+    def __eq__(self, other):
+        if self.space == other.space and self.representation == other.representation and self.rank == other.rank:
+            return True
+        else:
+            return False
 
     @property
     def rank(self):
@@ -99,22 +137,53 @@ class TensorOperator(TensorOperatorInterface):
         return self._space
 
     def add(self, other):
-        pass
+        if isinstance(other, self.CompositeClass):
+            return other + self
+        elif self.representation == other.representation and self.rank == other.rank:
+            new_factor = self.factor + other.factor
+            return self.__class__(self.rank, new_factor, self.space, self.representation)
+        else:
+            return self.CompositeClass(self, other)
+
+    def to_latex(self):
+        return str(self.factor) + " * "\
+               + str(self.representation).replace("[", "\\left\\lbrace").replace("]", "\\right\\rbrace").\
+                   replace(', ', ' \\otimes ').replace("'", "") + "_" + "{" + str(self.rank) + "}"
+
+    def to_expression(self):
+        return str(self.factor) + " * " + self._to_expression_no_factor()
+
+    def _to_expression_no_factor(self):
+        return str(self.representation).replace("[", "{").replace("]", "}").replace(', ', ' x ').replace("'", "") \
+               + "_" + str(self.rank)
 
     def couple(self, other, rank, factor):
-        return
+        if not abs(self.rank - other.rank) <= rank <= self.rank + other.rank:
+            return 0  # Coupling those two operators to the new rank is not possible
 
+        new_factor = self.factor * other.factor * factor
 
-"""    
-representation = "{" + tensorOne + " x " + tensorTwo + "}"
-
-        if tensorOne.space() == tensorTwo.space():
-            space = tensorOne.space()
+        if self.space == other.space:
+            new_space = self.space
         else:
-            space = "{" + tensorOne.space() + " x " + tensorTwo.space() + "}"
+            new_space = [self.space, other.space]
+            # TODO possibly I have to reorder here and use commutator relations
+        new_representation = [self._to_expression_no_factor(), other._to_expression_no_factor()]
 
-        super().__init__(rank, factor, space=space, representation=representation)
-        self._tex_representation = "\left\lbrace" + tensorOne + " \otimes " + tensorTwo + "\right\rbrace"
-        self.tensorOne = tensorOne
-        self.tensorTwo = tensorTwo
-"""
+        return self.__class__(rank, new_factor, new_space, new_representation)
+
+
+if __name__ == "__main__":
+    top = TensorOperator(rank=1, factor=1, space='rel', representation="q")
+    qsq = top.couple(top, 0, 3)
+    print(qsq)
+    print(top)
+
+    tlist = qsq + 2 * top
+    print(tlist)
+    print(top)
+
+    tlist = tlist + qsq + top
+    print(tlist)
+    tlist._simplify()
+    print(tlist)
--- testing/tensor_operator_unit_test.py
+++ testing/tensor_operator_unit_test.py
@@ -3,9 +3,39 @@ from tensor_operator import TensorOperator, TensorOperatorComposite
 import win_unicode_console
 
 class TestTensorOperator(unittest.TestCase):
+
+    basic_block = TensorOperator(rank=1, representation='q', space='relative')
+    other_block = TensorOperator(rank=1, representation='k', space='relative')
+
     def test_building_block(self):
-        basic_block = TensorOperator(rank=1, representation='q', space='relative')
-        self.assertEqual(basic_block.to_expression(), "q1")  # add assertion here
+        self.assertEqual("1 * q_1", str(self.basic_block))  # add assertion here
+
+    def test_couple(self):
+        self.assertEqual("2 * {q_1 x q_1}_2", str(self.basic_block.couple(self.basic_block, 2, 2)))
+        self.assertEqual("1 * {q_1 x k_1}_2", str(self.basic_block.couple(self.other_block, 2, 1)))
+        self.assertEqual("1 * {q_1 x k_1}_1", str(self.basic_block.couple(self.other_block, 1, 1)))
+        self.assertEqual("1 * {q_1 x k_1}_0", str(self.basic_block.couple(self.other_block, 0, 1)))
+        self.assertEqual("0", str(self.basic_block.couple(self.other_block, 3, 1)))  # 3 > 1 + 1, so not allowed
+        self.assertEqual("0", str(self.basic_block.couple(self.basic_block, 100, 1)))
+        self.assertEqual("0", str(self.basic_block.couple(self.basic_block, -1, 1)))
+
+    def test_couple_tensor_operator_list(self):
+        pass
+
+    def test_mul(self):
+        self.assertEqual("5.0 * q_1", str(self.basic_block * 5.))
+        self.assertEqual("2 * q_1", str(2 * self.basic_block))
+
+    def test_add_same(self):
+        self.assertEqual("2 * q_1", str(self.basic_block + self.basic_block))
+
+    def test_add_other(self):
+        self.assertEqual("1 * q_1 + 1 * k_1", str(self.basic_block + self.other_block))
+
+    def test_add_tensor_operator_list(self):
+        tensorList = self.basic_block + self.other_block
+        self.assertEqual("2 * q_1 + 1 * k_1", str(tensorList + self.basic_block))
+        self.assertEqual("2 * q_1 + 1 * k_1", str(self.basic_block + tensorList))
 
 
 if __name__ == '__main__':
