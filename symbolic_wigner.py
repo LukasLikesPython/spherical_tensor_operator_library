787ef2446a20b3056f7fd90b065875ea5e5897b8
Lukas <Lukas.Huth@gmx.net>
2022-09-21 20:00:53 +0200

    symbolic wigner

--- symbolic_wigner.py
+++ symbolic_wigner.py
@@ -8,6 +8,9 @@ class SymbolicWigner(ABC):
     An auxiliary construct that helps to hold the evaluation of wigner_6j and wigner_9j symbols.
     """
 
+    def __init__(self):
+        self.factor = None
+
     @abstractmethod
     def evaluate(self, *args, **kwargs):
         pass
@@ -33,10 +36,11 @@ class SymbolicWigner(ABC):
 class SymbolicWignerComposite(SymbolicWigner):
 
     def __init__(self, instance_1, instance_2):
+        super().__init__()
         self.children = []
         self.add_children(instance_1)
         self.add_children(instance_2)
-        self.factor = instance_1.factor * instance_2.factor
+        self.factor = 1
 
     def __str__(self):
         return f"{self.factor} * (" + " * ".join([str(child) for child in self.children]) + ")"
@@ -49,7 +53,11 @@ class SymbolicWignerComposite(SymbolicWigner):
             self.factor *= other
 
     def evaluate(self, symbol_replace_dict):
-        return self.factor.subs(symbol_replace_dict) # TODO
+        sub_symbol_value = 1
+        for child in self.children:
+            sub_symbol_value *= child.evaluate(symbol_replace_dict)
+        factor = self.factor.subs(symbol_replace_dict) if isinstance(self.factor, Symbol) else self.factor
+        return factor * sub_symbol_value
 
     def add_children(self, other):
         if isinstance(other, self.__class__):
@@ -60,7 +68,10 @@ class SymbolicWignerComposite(SymbolicWigner):
 
 class Symbolic6j(SymbolicWigner):
 
+    six_j_cache = {}
+
     def __init__(self, j1, j2, j3, j4, j5, j6, factor=1):
+        super().__init__()
         self.j1 = j1
         self.j2 = j2
         self.j3 = j3
@@ -79,13 +90,25 @@ class Symbolic6j(SymbolicWigner):
             self.factor *= other
             return self
 
-    def evaluate(self, j1, j2, j3, j4, j5, j6):
-        return self.factor * wigner_6j(j1, j2, j3, j4, j5, j6)
+    def evaluate(self, symbol_replace_dict):
+        j1, j2, j3, j4, j5, j6 = [x.subs(symbol_replace_dict) if isinstance(x, Symbol) else x for x in
+                                  (self.j1, self.j2, self.j3, self.j4, self.j5, self.j6, )]
+        factor = self.factor.subs(symbol_replace_dict) if isinstance(self.factor, Symbol) else self.factor
+        key = (j1, j2, j3, j4, j5, j6)
+        if key in self.six_j_cache:
+            six_j_value = self.six_j_cache[key]
+        else:
+            six_j_value = wigner_6j(j1, j2, j3, j4, j5, j6)
+            self.six_j_cache[key] = six_j_value
+        return factor * six_j_value
 
 
 class Symbolic9j(SymbolicWigner):
 
+    nine_j_cache = {}
+
     def __init__(self, j1, j2, j3, j4, j5, j6, j7, j8, j9, factor=1):
+        super().__init__()
         self.j1 = j1
         self.j2 = j2
         self.j3 = j3
@@ -108,6 +131,36 @@ class Symbolic9j(SymbolicWigner):
             self.factor *= other
             return self
 
-    def evaluate(self, j1, j2, j3, j4, j5, j6, j7, j8, j9):
-        return self.factor * wigner_9j(j1, j2, j3, j4, j5, j6, j7, j8, j9)
+    def evaluate(self, symbol_replace_dict):
+        j1, j2, j3, j4, j5, j6, j7, j8, j9 = [x.subs(symbol_replace_dict) if isinstance(x, Symbol) else x for x in
+                                              (self.j1, self.j2, self.j3, self.j4, self.j5, self.j6, self.j7, self.j8,
+                                               self.j9)]
+        factor = self.factor.subs(symbol_replace_dict) if isinstance(self.factor, Symbol) else self.factor
+        key = (j1, j2, j3, j4, j5, j6, j7, j8, j9)
+        if key in self.nine_j_cache:
+            nine_j_value = self.nine_j_cache[key]
+        else:
+            nine_j_value = wigner_9j(j1, j2, j3, j4, j5, j6, j7, j8, j9)
+            self.nine_j_cache[key] = nine_j_value
+        return factor * nine_j_value
 
+
+if __name__ == "__main__":
+    from sympy import Symbol
+
+    six_j = Symbolic6j(Symbol('j1'), Symbol('j2'), Symbol('j3'), Symbol('j4'), Symbol('j5'), 0)
+    subsdict = {Symbol('j1'): 1, Symbol('j2'): 1, Symbol('j3'): 0, Symbol('j4'): 1, Symbol('j5'): 1,
+                Symbol('ja'): 2, Symbol('jb'): 2, Symbol('jc'): 0, Symbol('jd'): 1, Symbol('je'): 1, Symbol('jf'): 2,
+                Symbol('jh'): 1, Symbol('jk'): 1, Symbol('ji'): 2}
+
+    print(six_j.evaluate(subsdict))
+    print(six_j)
+
+    nine_j = Symbolic9j(Symbol('ja'), Symbol('jb'), Symbol('jc'), Symbol('jd'), Symbol('je'), Symbol('jf'),
+                        Symbol('jh'), Symbol('jk'), Symbol('ji'), factor=2)
+    print(nine_j)
+    print(nine_j.evaluate(subsdict))
+
+    coupled = six_j * nine_j
+    print(coupled)
+    print(coupled.evaluate(subsdict))
