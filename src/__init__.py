3af1dfe4a9412bf9c9eafeec3f5bc036aa0c7e51
Lukas <Lukas.Huth@gmx.net>
2022-11-01 15:34:54 +0100

    tests with __init__ files

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+import stol.quantum_states
+import stol.symbolic_wigner
+import stol.tensor_algebra
+import stol.tensor_evaluate
+import stol.tensor_operator
+import stol.tensor_space
+import stol.tensor_transformation
--- src/stol/__init__.py
+++
@@ -1,3 +0,0 @@
-import tensor_space, tensor_operator, tensor_algebra, tensor_transformation
-import quantum_states, symbolic_wigner
-import tensor_evaluate
